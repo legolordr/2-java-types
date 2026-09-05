90df8953235dabe1ca40415827cb2538a2f80a9e
legolordr <Ishmametev_den@mail.ru>
2026-09-05 19:58:47 +0500

    Ишмаметьев Денис. Задание 14

--- task14/src/main/java/com/example/task14/Task14Main.java
+++ task14/src/main/java/com/example/task14/Task14Main.java
@@ -5,9 +5,9 @@ public class Task14Main {
 
     public static int reverse(int value) {
 
-        // TODO напишите здесь код, переставляющий цифры числа в обратном порядке
-
-        return 0;
+        String str = Integer.toString(value);
+        str =  new StringBuilder(str).reverse().toString();
+        return Integer.parseInt(str);
     }
 
     public static void main(String[] args) {
